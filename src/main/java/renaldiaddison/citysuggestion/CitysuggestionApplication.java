6b936541dfd754e8459d37924049d474fda738c9
renaldiaddison <addisonrenaldi@gmail.com>
2024-11-26 10:01:05 +0700

    Initial commit

---
+++ src/main/java/renaldiaddison/citysuggestion/CitysuggestionApplication.java
@@ -0,0 +1,13 @@
+package renaldiaddison.citysuggestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitysuggestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitysuggestionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/renaldiaddison/citysuggestion/CitysuggestionApplicationTests.java
@@ -0,0 +1,13 @@
+package renaldiaddison.citysuggestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitysuggestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
